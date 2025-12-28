b13b6dcd623877380311d2588eb8b5c437f9daf5
Xgui4 Studio <134389196+xgui4@users.noreply.github.com>
2025-12-28 02:25:21 -0500

    installer updated and old installer file removed

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-echo "HyperArch Dotfiles Installer - Prototype Version"
+echo "Xgui4 OS Dotfiles Installer - Prototype 2 Version"
 
 # Variables
 RED="\e[31m"
@@ -8,44 +8,14 @@ ENDCOLOR="\e[0m"
 
 echo "This bash script is experimental and a work-in-progress !"
 
-read -r -p 'Do you want to install yay AUR helper. This is required to install AUR packages via this script (Y/N) (may be already installed on your system) ?' confirm_yay
-if [[ $confirm_yay == "Y" || $confirm_yay == "y"]]; then
-    # Install yay AUR helper
-    sudo pacman -S --needed git base-devel || echo -e "${RED}[Error]${ENDCOLOR} Failed to install git and base-devel packages."
-    git clone https://aur.archlinux.org/yay.git || echo -e "${RED}[Error]${ENDCOLOR} Failed to clone yay repository."
-    cd yay || echo -e "${RED}[Error]${ENDCOLOR} Failed to change directory to yay."
-    makepkg -si || echo -e "${RED}[Error]${ENDCOLOR} Failed to build and install yay."
-    cd .. || echo -e "${RED}[Error]${ENDCOLOR} Failed to change directory back."
-    rm -rf yay || echo -e "${RED}[Error]${ENDCOLOR} Failed to remove yay directory."
-fi
+# installer for stuff temporaly removed as it was obselete
 
-read -r -p 'Do you want to update the system before installing the apps (Recommended) (Y/N) ?' confirm
+mkdir ~/.config-backup
 
-# Update the system (recommended)
-if [[ $confirm == "Y" || $confirm == "y" ]]; then
-    sudo pacman -Syu
-    yay -Syu || echo -e "${RED}[Error]${ENDCOLOR} yay is not installed or have failed to run."
-fi
+cp -a .config/ ~/.config-backup/
 
-# Installing packages with pacman
-sudo pacman -S --needed $(< pacman-essentials) || echo -e "${RED}[Error]${ENDCOLOR} Some packages from pacman-essentials have failed to install."
+mkdir ~/.config
 
-read -r -p 'Do you want to install additional applications (Y/N) ?' confirm_apps
-if [[ $confirm_apps == "Y" || $confirm_apps == "y" ]]; then
-    sudo pacman -S --needed $(< pacman-apps) || echo -e "${RED}[Error]${ENDCOLOR} Some packages from pacman-apps have failed to install."
+cp -a fastfetch/ fish/ gtk-3.0/ gtk-4.0/ hypr/ i3/ kitty/ nwg-look/ picom/ polybar/ qt5ct/ qtile/ rofi/ wallpapers/ waybar/ ~/.config/ 
 
-    # Installing packages from the AUR
-    yay -S $(< aur) || echo -e "${RED}[Error]${ENDCOLOR} yay is not installed or have failed to run."
-fi
-
-# Enable sddm login manager
-sudo systemctl enable sddm.service || echo -e "${RED}[Error]${ENDCOLOR} Failed to enable sddm service."
-
-read -o -p 'Do you want to Install XLibre ? (Y/N) ?' confirm_xlibre
-if [[ $confirm_xlibre == "Y" || $confirm_xlibre == "y" ]]; then
-    curl -O https://x11libre.net/repo/arch_based/x86_64/install-xlibre.sh 
-    sudo chmod +x install-xlibre.sh
-    sudo ./install-xlibre.sh || echo -e "${RED}[Error]${ENDCOLOR} XLibre installation script has failed."
-fi
-
-cp -r * ~/.config/ || echo -e "${RED}[Error]${ENDCOLOR} Failed to copy dotfiles to ~/.config/."
+echo "This script is still not finished, some must be put manually for now"
