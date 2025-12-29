2b8950d06d3464db6881620c7ed69444bcd53988
Xgui4 Studio <134389196+xgui4@users.noreply.github.com>
2025-12-28 22:53:50 -0500

    make the script work

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
 
+echo "WARNING: this script is a work in progress and have not being tested"
+
 echo "I recommend XLibre"
 
-
-# read -o -p 'So Do you want to Install XLibre ? (Y/N) ?' confirm_xlibre
-# if [[ $confirm_xlibre == "Y" || $confirm_xlibre == "y" ]]; then
-  #  curl -O https://x11libre.net/repo/arch_based/x86_64/install-xlibre.sh 
-  # sudo chmod +x install-xlibre.sh
-  #  sudo ./install-xlibre.sh || echo -e "${RED}[Error]${ENDCOLOR} XLibre installation script has failed."
-# fi
+read -o -p 'So Do you want to Install XLibre ? (Y/N) ?' confirm_xlibre
+if [[ $confirm_xlibre == "Y" || $confirm_xlibre == "y" ]]; then
+  curl -O https://x11libre.net/repo/arch_based/x86_64/install-xlibre.sh 
+  sudo chmod +x install-xlibre.sh
+  sudo ./install-xlibre.sh || echo -e "${RED}[Error]${ENDCOLOR} XLibre installation script has failed."
+fi
 
 # Define the JSON file
 ARCH="arch-essential-pkg.json"
@@ -17,22 +18,22 @@ HYPRARCH="hyprarch-setup.json"
 QTILE="qtile-setup.json"
 
 packages=($(jq -r '.pacman[]' "$ARCH"))
-echo "pacman:" ${packages[@]}
+sudo pacman -S ${packages[@]}
 
 aur=($(jq -r '.aur[]' "$ARCH"))
-echo "AUR:" ${aur[@]}
+yay -S ${aur[@]}
 
 packages=($(jq -r '.pacman[]' "$I3"))
-echo "pacman: "${packages[@]}
+sudo pacman -S ${packages[@]}
 
 packages=($(jq -r '.pacman[]' "$HYPRARCH"))
-echo  "pacman:" ${packages[@]}
+sudo pacman -S ${packages[@]}
 
 aur=($(jq -r '.aur[]' "$HYPRARCH"))
-echo "AUR:" ${aur[@]}
+yay -S ${aur[@]}
 
 packages=($(jq -r '.pacman[]' "$QTILE"))
-echo "pacman:"${packages[@]}
+sudp pacman -S ${packages[@]}
 
 aur=($(jq -r '.aur[]' "$QTILE"))
-echo  "AUR:" ${aur[@]}
+yay -S ${aur[@]}
