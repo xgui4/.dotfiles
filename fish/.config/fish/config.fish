0cec66c5e1d4a047d0980919ccafaf4b10204a2b
Xgui4 Studio <134389196+xgui4@users.noreply.github.com>
2026-01-26 18:20:41 -0500

    Better Shells Experience Tests - January 25 2026 - 18h25
    - Added Exa, Ugrep, Bat and replace by default their coreutils command and replace their command with alias with a g in the fish shell
    - Added those pkg into the arch essentials jsons files
    - Fixed some hyprland changes
    - Temporaly disabled the maxisum window border color change temporaly until i fixed it for the new hyprland version

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
-    alias ls="ls --color=auto -A"
-    alias grep="grep --color=auto"
-    alias fastfetch-image="fastfetch -c ~/.config/fastfetch/config-image.jsonc"
-    alias i-use-arch-btw="fastfetch -c ~/.config/fastfetch/config-image.jsonc --logo arch"
+    alias fastfetch-image="fastfetch -c ~/.dotfiles/fastfetch/.config/fastfetch/config-image.jsonc"
+    alias i-use-arch-btw="fastfetch -c ~/.dotfiles/fastfetch/.config//fastfetch/config-image.jsonc --logo arch"
     alias wkill="hyprctl kill"
     alias neofetch="fastfetch -c neofetch"
     alias cmatrix-rainbow="cmatrix | lolcat"
-    alias edit="code"
+    alias cat="bat"
+    alias gcat="/usr/bin/cat"
+    alias ls="exa --icons=auto"
+    alias gls="/usr/bin/ls"
+    alias grep="ugrep"
+    alias ggrep="/usr/bin/grep"
 end
 
 function mkcd --description "Create a directory and cd into it"
