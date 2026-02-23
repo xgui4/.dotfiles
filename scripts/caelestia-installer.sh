00d29f7de76d52c7ddf5a0eb68a499b5c30a4795
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-02-23 11:34:37 -0500

    some minors changes to installer and startship

--- scripts/caelestia-installer.sh
+++ scripts/caelestia-installer.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd .. 
 
-cd quickshell/.config/quickshell || echo "error to go to the quickshell folder" && exit
+cd quickshell/.config/quickshell
 
 git clone https://github.com/xgui4/caelestia-shell.git shell
 
-cd shell || echo "Error: Could not install the caelestia-shell dotfiles" && exit
+cd shell
 
 # Compile  my fork of caelestia-shell
 cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/
 cmake --build build
 sudo cmake --install build
 
-sudo cp caelestia-shell.desktop  /usr/share/wayland-sessions/caelestia-shell.desktop
+sudo cp assets/wayland-sessions/caelestia-shell.desktop  /usr/share/wayland-sessions/caelestia-shell.desktop
