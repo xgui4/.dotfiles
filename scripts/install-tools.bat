2ac62f158e5bf3176ee91f64c4751c8eeac0e9f8
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-05-10 19:30:53 -0400

    update dotfiles

--- scripts/install-tools.bat
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-ln -s ~/.dotfiles/scripts/xgui4-os-dotfiles-version /usr/bin/xgui4-os-dotfiles-version
--- scripts/xgui4-os-dotfiles-version.bat
+++ scripts/xgui4-os-dotfiles-version.bat
@@ -1,7 +1,1 @@
-#!/usr/bin/env sh
-
-if [ -f "$HOME/.dotfiles/VERSION" ]; then
-    cat ~/.dotfiles/VERSION
-else
-    echo "Xgui4 OS Dotfiles not installed."
-fi
+type $HOME/.dotfiles/VERSION.txt
