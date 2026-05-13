9489b2d640c60e57069e0ce8dc41fafb1a308081
Xgui4 Studio <134389196+xgui4@users.noreply.github.com>
2026-05-13 00:52:08 -0400

    fix hyprland config

--- hyprland/.config/hypr/new-lua-config/plugins.lua
+++ hyprland/.config/hypr/new-lua-config/plugins.lua
@@ -1,1 +1,24 @@
+hl.config({
+    plugin = {
+        hyprbars = {
+            bar_height = 20,
+            on_double_click = "hyprctl dispatch fullscreen 1",
+        },
+    },
+})
 
+hl.plugin.hyprbars.add_button({
+    bg_color = "rgb(ff4040)",
+    fg_color = "rgb(ffffff)",
+    size = 10,
+    icon = "X",
+    action = "hyprctl dispatch killactive",
+})
+
+hl.plugin.hyprbars.add_button({
+    bg_color = "rgb(eeee11)",
+    fg_color = "rgb(000000)",
+    size = 10,
+    icon = "_",
+    action = "hyprctl dispatch fullscreen 1",
+})
